5e62385f64b6c54f9616f608230c7ca1e34c49c4
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-05-11 21:49:15 +1000

    Update eq_gain_number.cpp

--- components/tas5805m/number/eq_gain_number.cpp
+++ components/tas5805m/number/eq_gain_number.cpp
@@ -5,7 +5,7 @@ namespace tas5805m {
 
 void EqGainNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_eq_gain(0, static_cast<int>value);
+  this->parent_->set_eq_gain(0, static_cast<int>(value));
 }
 
 }  // namespace tas5805m
