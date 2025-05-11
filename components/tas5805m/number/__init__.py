4b5cf81ff78b812b84793d811e42d8a3e01a44a2
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-05-12 00:06:43 +1000

    Update __init__.py

--- components/tas5805m/number/__init__.py
+++ components/tas5805m/number/__init__.py
@@ -35,13 +35,9 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
-    if CONF_EQ in config:
-        #cg.add(tas5805m_component.config_eq(True))
-        if gain_20Hz_config := config.get(CONF_GAIN_20HZ):
-            n = await number.new_number(
-                gain_20Hz_config, min_value=-15, max_value=15, step=1
-            )
-            await cg.register_parented(n, tas5805m_component)
-            cg.add(tas5805m_component.set_gain_20_hz_number(n))
-    #else:
-        #cg.add(tas5805m_component.config_eq(False))
+    if gain_20hz_config := config.get(CONF_GAIN_20HZ):
+        n = await number.new_number(
+            gain_20hz_config, min_value=-15, max_value=15, step=1
+        )
+        await cg.register_parented(n, tas5805m_component)
+        cg.add(tas5805m_component.set_gain_20_hz_number(n))
