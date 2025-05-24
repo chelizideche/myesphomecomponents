77cffb02eba2a8e55c98d7ba7bdf0959ed2a36a5
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-05-24 14:26:20 +1000

    Update tas5805m.h

--- components/tas5805m/tas5805m.h
+++ components/tas5805m/tas5805m.h
@@ -5,8 +5,10 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/hal.h"
 
+#ifdef USE_NUMBER
 #include "tas5805m_cfg.h"
 #include "tas5805m_eq.h"
+#endif
 
 namespace esphome {
 namespace tas5805m {
@@ -18,7 +20,7 @@ class Tas5805mComponent : public audio_dac::AudioDac, public Component, public i
   void setup() override;
   void loop() override;
   void dump_config() override;
-  float get_setup_priority() const override { return setup_priority::IO; }
+  float get_setup_priority() const override { return setup_priority::390.0f; }
 
   void set_enable_pin(GPIOPin *enable) { this->enable_pin_ = enable; }
   void config_analog_gain(float analog_gain) { this->analog_gain_ = analog_gain; }
@@ -33,11 +35,13 @@ class Tas5805mComponent : public audio_dac::AudioDac, public Component, public i
   bool set_deep_sleep_off();
   bool set_deep_sleep_on();
 
+  #ifdef USE_NUMBER
   bool set_eq_on();
   bool set_eq_off();
   bool set_eq_gain(uint8_t band, int8_t gain);
   int8_t eq_gain(uint8_t band);
   void refresh_eq_gains();
+  #endif
 
  protected:
    GPIOPin *enable_pin_{nullptr};
@@ -56,6 +60,7 @@ class Tas5805mComponent : public audio_dac::AudioDac, public Component, public i
    // reads analog gain register and returns number 0-31
    bool get_analog_gain(uint8_t* raw_gain);
 
+   #ifdef USE_NUMBER
    bool get_dac_mode(Tas5805mDacMode* mode);
    bool get_eq(bool* enabled);
    bool get_modulation_mode(Tas5805mModMode* mode, Tas5805mSwFreq* freq, Tas5805mBdFreq* bd_freq);
@@ -63,8 +68,8 @@ class Tas5805mComponent : public audio_dac::AudioDac, public Component, public i
    bool get_bck_ratio(uint8_t* ratio);
    bool get_power_state(Tas5805mControlState* state);
    bool set_eq(bool enable);
-
    bool set_book_and_page(uint8_t book, uint8_t page);
+   #endif
 
    bool tas5805m_read_byte(uint8_t a_register, uint8_t* data);
    bool tas5805m_write_byte(uint8_t a_register, uint8_t data);
@@ -79,14 +84,19 @@ class Tas5805mComponent : public audio_dac::AudioDac, public Component, public i
    struct Tas5805mState {
     //bool                 is_muted;                   // not used as esphome AudioDac component has its own is_muted variable
     bool                   is_powered;
-    bool                   eq_enabled;
+    Tas5805mControlState   state;
+    #ifdef USE_NUMBER
+    bool                   eq_enabled{false};
     int8_t                 eq_gain[TAS5805M_EQ_BANDS]{0};
     bool                   eq_gain_set[TAS5805M_EQ_BANDS]{false};
-    Tas5805mControlState   state;
     Tas5805mMixerMode      mixer_mode;
+    #endif
    } tas5805m_state_;
 
+   #ifdef USE_NUMBER
    bool run_refresh_eq_gains_{false};
+   uint8_t refresh_band_{0};
+   #endif
 
    float volume_{0};
 
@@ -95,8 +105,6 @@ class Tas5805mComponent : public audio_dac::AudioDac, public Component, public i
    uint8_t i2c_error_{0};
 
    uint8_t digital_volume_{0};
-
-   uint8_t refresh_band_{0};
 
    uint16_t number_registers_configured_{0};
 };
