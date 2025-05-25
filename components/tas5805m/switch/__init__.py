12301c6b147b2192139f1df554f1f79132f5e92f
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-05-25 23:06:52 +1000

    add component to switch

--- components/tas5805m/switch/__init__.py
+++ components/tas5805m/switch/__init__.py
@@ -7,8 +7,8 @@ from ..audio_dac import CONF_TAS5805M_ID, Tas5805mComponent, tas5805m_ns
 CONF_ENABLE_DAC = "enable_dac"
 CONF_ENABLE_EQ = "enable_eq"
 
-EnableDacSwitch = tas5805m_ns.class_("EnableDacSwitch", switch.Switch)
-EnableEqSwitch = tas5805m_ns.class_("EnableEqSwitch", switch.Switch)
+EnableDacSwitch = tas5805m_ns.class_("EnableDacSwitch", switch.Switch, cg.Component)
+EnableEqSwitch = tas5805m_ns.class_("EnableEqSwitch", switch.Switch , cg.Component)
 
 # CONFIG_SCHEMA = switch.switch_schema(
 #     EnableSwitch,
@@ -27,12 +27,14 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Optional(CONF_ENABLE_DAC): switch.switch_schema(
             EnableDacSwitch,
             device_class=DEVICE_CLASS_SWITCH,
-        ),
+        )
+        .extend(cv.COMPONENT_SCHEMA),
 
         cv.Optional(CONF_ENABLE_EQ): switch.switch_schema(
             EnableEqSwitch,
             device_class=DEVICE_CLASS_SWITCH,
-        ),
+        )
+        .extend(cv.COMPONENT_SCHEMA),
     }
 )
 
@@ -40,9 +42,11 @@ async def to_code(config):
     tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
     if enable_dac_config := config.get(CONF_ENABLE_DAC):
         s = await switch.new_switch(enable_dac_config)
+        await cg.register_component(s, enable_dac_config)
         await cg.register_parented(s, config[CONF_TAS5805M_ID])
         cg.add(tas5805m_component.set_enable_dac_switch(s))
     if enable_eq_config := config.get(CONF_ENABLE_EQ):
         s = await switch.new_switch(enable_eq_config)
+        await cg.register_component(s, enable_eq_config)
         await cg.register_parented(s, config[CONF_TAS5805M_ID])
         cg.add(tas5805m_component.set_enable_eq_switch(s))
--- components/tas5805m/switch/enable_eq_switch.h
+++ components/tas5805m/switch/enable_eq_switch.h
@@ -7,6 +7,9 @@ namespace esphome {
 namespace tas5805m {
 
 class EnableEqSwitch : public switch_::Switch, public Parented<Tas5805mComponent> {
+public:
+  float get_setup_priority() const override { return setup_priority::PROCESSOR; }
+
  protected:
   void write_state(bool state) override;
 };
