af59c9a73d2ce794944095b6e4ac5d74dcf835d2
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-05-26 08:22:18 +1000

    Revert "include and extend class for  component"
    This reverts commit f87b636188260448e6ef91a1e4cc641a80f9c344.

--- components/tas5805m/switch/enable_eq_switch.h
+++ components/tas5805m/switch/enable_eq_switch.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "esphome/components/switch/switch.h"
-#include "esphome/core/component.h"
 #include "../tas5805m.h"
 
 namespace esphome {
 namespace tas5805m {
 
-class EnableEqSwitch : public switch_::Switch, public Component, public Parented<Tas5805mComponent> {
+class EnableEqSwitch : public switch_::Switch, public Parented<Tas5805mComponent> {
 public:
   float get_setup_priority() const override { return setup_priority::PROCESSOR; }
 
