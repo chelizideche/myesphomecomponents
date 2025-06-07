24cd5b06a24075ddaad834861e96b3a967efcd73
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-06-07 23:13:52 +1000

    Update tas5805m_sensor.cpp

--- components/tas5805m/sensor/tas5805m_sensor.cpp
+++ components/tas5805m/sensor/tas5805m_sensor.cpp
@@ -26,13 +26,29 @@ void  Tas5805mSensor::update() {
     }
   }
   if (this->last_channel_fault_sensor_ != nullptr) {
-    if ( (uint8_t)(this->last_channel_fault_sensor_->get_state()) != this->parent_->last_channel_fault() ) {
-      this->last_channel_fault_sensor_->publish_state(this->parent_->last_channel_fault());
+    uint8_t new_fault_code = this->parent_->last_channel_fault();
+    if ( (uint8_t)(this->last_channel_fault_sensor_->get_state()) != new_fault_code ) {
+      this->last_channel_fault_sensor_->publish_state(new_fault_code);
+      if (new_fault_code) {
+        if (new_fault_code & (1 << 0)) ESP_LOGW(TAG, "Tas5805m Right channel over current fault");
+        if (new_fault_code & (1 << 1)) ESP_LOGW(TAG, "Tas5805m Left channel over current fault");
+        if (new_fault_code & (1 << 2)) ESP_LOGW(TAG, "Tas5805m Right channel DC fault");
+        if (new_fault_code & (1 << 3)) ESP_LOGW(TAG, "Tas5805m Left channel DC fault");
+      }
     }
   }
   if (this->last_global_fault_sensor_ != nullptr) {
-    if ( (uint8_t)(this->last_global_fault_sensor_->get_state()) != this->parent_->last_global_fault() ) {
-      this->last_global_fault_sensor_->publish_state(this->parent_->last_global_fault());
+    uint8_t new_fault_code = this->parent_->last_global_fault();
+
+    if ( (uint8_t)(this->last_global_fault_sensor_->get_state()) != new_fault_code ) {
+      this->last_global_fault_sensor_->publish_state(new_fault_code);
+      if (new_fault_code) {
+        if (new_fault_code & (1 << 0)) ESP_LOGW(TAG, "Tas5805m PVDD under-voltage fault");
+        if (new_fault_code & (1 << 1)) ESP_LOGW(TAG, "Tas5805m PVDD over-voltage fault");
+        if (new_fault_code & (1 << 2)) ESP_LOGW(TAG, "Tas5805m Clock fault");
+        if (new_fault_code & (1 << 6)) ESP_LOGW(TAG, "Recent BQ write failed");
+        if (new_fault_code & (1 << 7)) ESP_LOGW(TAG, "OTP CRC check error");
+      }
     }
   }
 }
