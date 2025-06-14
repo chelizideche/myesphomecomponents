a028ffbf877f072376ae034de2cdc1a7803e4151
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-06-14 17:00:24 +1000

    Update __init__.py

--- components/tas5805m/select/__init__.py
+++ components/tas5805m/select/__init__.py
@@ -22,40 +22,15 @@ CONFIG_SCHEMA = {
     ),
 }
 
-def find_component_config(component_id, domain=None):
-    """Helper function to find a component's configuration by ID"""
-    full_config = CORE.config
-
-    if domain:
-        # Search in specific domain
-        domain_config = full_config.get(domain, [])
-        if not isinstance(domain_config, list):
-            domain_config = [domain_config]
-
-        for item in domain_config:
-            if isinstance(item, dict) and item.get(CONF_ID) == component_id:
-                return item
-    else:
-        # Search all domains
-        for domain_name, domain_config in full_config.items():
-            if isinstance(domain_config, list):
-                for item in domain_config:
-                    if isinstance(item, dict) and item.get(CONF_ID) == component_id:
-                        return item
-            elif isinstance(domain_config, dict) and domain_config.get(CONF_ID) == component_id:
-                return domain_config
-
-    return None
-
 async def to_code(config):
     tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
     mixer_mode_config = config.get(CONF_MIXER_MODE)
     # Get ID for the output speaker and add it to the source speakrs config to easily inherit properties
      # You can also access the referenced component's config here
     # This is useful for generating different code based on the other component
-    ref_config = find_component_config(tas5805m_component)
-    dac_config = ref_config.get(CONF_DAC_MODE)
-    if ref_config and dac_config and config[CONF_DAC_MODE] == CONF_PBTL:
+    tas5805m_config = tas5805m_component.config
+    dac_config = tas5805m_config.get(CONF_DAC_MODE)
+    if dac_config == CONF_PBTL:
         s = await select.new_select(
                 mixer_mode_config,
                 options=["MONO", "RIGHT", "LEFT"],
