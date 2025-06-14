95dde4ee6c10d2233490b081fab246ad3a7a3a3d
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-06-14 20:56:43 +1000

    Update __init__.py

--- components/tas5805m/select/__init__.py
+++ components/tas5805m/select/__init__.py
@@ -3,10 +3,11 @@ from esphome.components import select
 import esphome.config_validation as cv
 from esphome.const import ENTITY_CATEGORY_CONFIG
 from ..audio_dac import CONF_TAS5805M_ID, Tas5805mComponent, tas5805m_ns
-
+import esphome.final_validate as fv
 from esphome.const import CONF_ID
 
 MixerModeSelect = tas5805m_ns.class_("MixerModeSelect", select.Select)
+CONF_AUDIO_DAC = "audio_dac"
 CONF_MIXER_MODE = "mixer_mode"
 CONF_DAC_MODE = "dac_mode"
 CONF_PBTL = "PBTL"
@@ -21,14 +22,16 @@ CONFIG_SCHEMA = {
 
 async def to_code(config):
     tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
-    tas5805m_config = tas5805m_component.config
-    dac_config = tas5805m_config[CONF_DAC_MODE]
-    raise cv.Invalid(
-            f"DAC MODE set to {dac_config} ok"
+    #tas5805m_config = tas5805m_component.config
+    #dac_config = tas5805m_config[CONF_DAC_MODE]
+    audio_dac_conf = fv.full_config.get()[CONF_AUDIO_DAC]
+    if audio_dac_conf[CONF_DAC_MODE] == CONF_PBTL:
+      raise cv.Invalid(
+            f"DAC MODE set to PBTL ok"
         )
     mixer_mode_config = config.get(CONF_MIXER_MODE)
 
-    if dac_config == CONF_PBTL:
+    if audio_dac_conf[CONF_DAC_MODE] == CONF_PBTL:
         s = await select.new_select(
                 mixer_mode_config,
                 options=["MONO", "RIGHT", "LEFT"],
