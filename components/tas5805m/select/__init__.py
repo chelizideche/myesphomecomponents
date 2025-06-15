4398fed293fe326563120d959e77650a5f07a47c
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-06-15 20:10:51 +1000

    Update __init__.py

--- components/tas5805m/select/__init__.py
+++ components/tas5805m/select/__init__.py
@@ -1,19 +1,19 @@
 import esphome.codegen as cg
 from esphome.components import select
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, ENTITY_CATEGORY_CONFIG
+from esphome.const import ENTITY_CATEGORY_CONFIG
 
 from ..audio_dac import CONF_TAS5805M_ID, Tas5805mComponent, tas5805m_ns
 
 MixerPresetSelect = tas5805m_ns.class_("MixerPresetSelect", select.Select)
 
-CONF_MIXER_MODE = "mixer_preset"
+CONF_MIXER_PRESET = "mixer_preset"
 
 CONFIG_SCHEMA = (
     cv.Schema(
       {
             cv.GenerateID(CONF_TAS5805M_ID): cv.use_id(Tas5805mComponent),
-            cv.Required(CONF_MIXER_MODE): select.select_schema(
+            cv.Required(CONF_MIXER_PRESET): select.select_schema(
                 MixerPresetSelect,
                 entity_category=ENTITY_CATEGORY_CONFIG,
             )
@@ -24,7 +24,7 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
-    mixer_preset_config = config.get(CONF_MIXER_MODE)
+    mixer_preset_config = config.get(CONF_MIXER_PRESET)
     s = await select.new_select(
             mixer_preset_config,
             options=["STEREO", "STEREO_INVERSE", "MONO", "RIGHT", "LEFT"],
