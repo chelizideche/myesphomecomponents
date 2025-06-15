3b3724990956607ea8ba2b2b4b7800e94212be52
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-06-15 23:00:23 +1000

    Update __init__.py

--- components/tas5805m/select/__init__.py
+++ components/tas5805m/select/__init__.py
@@ -22,7 +22,7 @@ async def to_code(config):
     mixer_mode_config = config.get(CONF_MIXER_MODE)
     s = await select.new_select(
             mixer_mode_config,
-            options=["STEREO", "STEREO_INVERSE", "MONO", "RIGHT", "LEFT"],
+            options=["STEREO", "LEFT"],
     )
     await cg.register_parented(s, config[CONF_TAS5805M_ID])
     cg.add(tas5805m_component.set_mixer_mode_select(s))
