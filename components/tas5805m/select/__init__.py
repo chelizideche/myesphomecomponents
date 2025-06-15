65f8e22aea5e10357a6591958e7a49bc53e221b2
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-06-15 21:54:32 +1000

    Update __init__.py

--- components/tas5805m/select/__init__.py
+++ components/tas5805m/select/__init__.py
@@ -1,42 +1,44 @@
 import esphome.codegen as cg
 from esphome.components import select
 import esphome.config_validation as cv
-from esphome.const import ENTITY_CATEGORY_CONFIG
+from esphome.const import CONF_ID, ENTITY_CATEGORY_CONFIG
 
 from ..audio_dac import CONF_TAS5805M_ID, Tas5805mComponent, tas5805m_ns
 
 MixerPresetSelect = tas5805m_ns.class_("MixerPresetSelect", select.Select, cg.Component)
 
-#CONF_MIXER_PRESET = "mixer_preset"
+CONF_MIXER_PRESET = "mixer_preset"
 
-# CONFIG_SCHEMA = (
-#     cv.Schema(
-#       {
-#             cv.GenerateID(CONF_TAS5805M_ID): cv.use_id(Tas5805mComponent),
-#             cv.Required(CONF_MIXER_PRESET): select.select_schema(
-#                 MixerPresetSelect,
-#                 entity_category=ENTITY_CATEGORY_CONFIG,
-#             )
-#             .extend(cv.COMPONENT_SCHEMA),
-#       }
-#     )
-# )
-CONFIG_SCHEMA = cv.All(
-    select.select_schema( MixerPresetSelect)
-    .extend(
-        {
+CONFIG_SCHEMA = (
+    cv.Schema(
+      {
+            cv.GenerateID(): cv.declare_id(MixerPresetSelect),
             cv.GenerateID(CONF_TAS5805M_ID): cv.use_id(Tas5805mComponent),
-        }
+            cv.Required(CONF_MIXER_PRESET): select.select_schema(
+                MixerPresetSelect,
+                entity_category=ENTITY_CATEGORY_CONFIG,
+            )
+            .extend(cv.COMPONENT_SCHEMA),
+      }
     )
-    .extend(cv.COMPONENT_SCHEMA),
 )
+# CONFIG_SCHEMA = cv.All(
+#     select.select_schema( MixerPresetSelect)
+#     .extend(
+#         {
+#             cv.GenerateID(CONF_TAS5805M_ID): cv.use_id(Tas5805mComponent),
+#         }
+#     )
+#     .extend(cv.COMPONENT_SCHEMA),
+# )
 
 async def to_code(config):
-    s = await select.new_select(
+    tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await cg.register_parented(var, tas5805m_component)
+    await select.new_select(
             config,
             options=["STEREO", "STEREO_INVERSE", "MONO", "RIGHT", "LEFT"],
     )
-    await cg.register_component(s, config)
 
-    tas5805m_component = await cg.get_variable(config[CONF_TAS5805M_ID])
-    await cg.register_parented(s, tas5805m_component)
